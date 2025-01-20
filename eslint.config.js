e940c158f16f854a9789b044580d0b9211168e1b
nicholascannon <nicholascannon1@gmail.com>
2025-01-20 20:02:54 +1100

    adjust linting

--- eslint.config.js
+++ eslint.config.js
@@ -81,7 +81,7 @@ export default tseslint.config(
 
             // Allow '_' prefixed variables to be unused
             '@typescript-eslint/no-unused-vars': 'off',
-            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-imports': 'warning',
             'unused-imports/no-unused-vars': [
                 'warn',
                 {
