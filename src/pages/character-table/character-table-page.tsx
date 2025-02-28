bb35c8b23d3313f7c1474fc6c032f0cb1016f466
nicholascannon <nicholascannon1@gmail.com>
2025-03-01 07:13:05 +0800

    Add basic prev next pagination for character table

--- src/pages/character-table/character-table-page.tsx
+++ src/pages/character-table/character-table-page.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react';
-import { Link } from 'react-router';
+import { useEffect } from 'react';
+import { Link, useSearchParams } from 'react-router';
 
 import { useGetCharacters } from './hooks/use-get-characters';
 import { isCharacter } from './utils/is-character';
 
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import {
+    Pagination,
+    PaginationContent,
+    PaginationItem,
+    PaginationNext,
+    PaginationPrevious,
+} from '@/components/ui/pagination';
 import { Skeleton } from '@/components/ui/skeleton';
 import {
     Table,
@@ -16,52 +23,132 @@ import {
 } from '@/components/ui/table';
 
 export function CharacterTablePage() {
-    const [page] = useState(1);
+    const [searchParams] = useSearchParams();
+    const page = Number(searchParams.get('page') ?? 1);
+
     const { data, error } = useGetCharacters(page);
+
+    useEffect(() => {
+        scrollToTop();
+    }, [page]);
 
     if (error) return <h1>Error: {error.message}</h1>;
 
     const characters = data?.characters?.results;
+    const pagination = data?.characters?.info;
+
+    const getPageLink = (page: number) => {
+        const newParams = new URLSearchParams(searchParams);
+        newParams.set('page', String(page));
+        return `/?${newParams.toString()}`;
+    };
+
+    const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' });
 
     return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Name</TableHead>
-                </TableRow>
-            </TableHeader>
+        <section>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Name</TableHead>
+                    </TableRow>
+                </TableHeader>
 
-            <TableBody>
-                {characters
-                    ? characters.filter(isCharacter).map((character) => (
-                          <TableRow key={character.id}>
-                              <TableCell className="flex flex-row items-center">
-                                  <Avatar>
-                                      <AvatarImage
-                                          src={
-                                              character.image ?? '/headshot.jpg'
-                                          }
-                                          alt={character.name ?? 'Character'}
-                                      />
-                                  </Avatar>
+                <TableBody>
+                    {characters
+                        ? characters.filter(isCharacter).map((character) => (
+                              <TableRow key={character.id}>
+                                  <TableCell className="flex flex-row items-center">
+                                      <Avatar>
+                                          <AvatarImage
+                                              src={
+                                                  character.image ??
+                                                  '/headshot.jpg'
+                                              }
+                                              alt={
+                                                  character.name ?? 'Character'
+                                              }
+                                          />
+                                      </Avatar>
 
-                                  <Link
-                                      to={`/character/${character.id}`}
-                                      className="ml-4"
-                                  >
-                                      {character.name ?? 'Unknown character'}
-                                  </Link>
-                              </TableCell>
-                          </TableRow>
-                      ))
-                    : new Array(10).fill(null).map((_, index) => (
-                          <TableRow key={index}>
-                              <TableCell>
-                                  <Skeleton className="h-[20px] w-[150px]" />
-                              </TableCell>
-                          </TableRow>
-                      ))}
-            </TableBody>
-        </Table>
+                                      <Link
+                                          to={`/character/${character.id}`}
+                                          className="ml-4"
+                                      >
+                                          {character.name ??
+                                              'Unknown character'}
+                                      </Link>
+                                  </TableCell>
+                              </TableRow>
+                          ))
+                        : new Array(10).fill(null).map((_, index) => (
+                              <TableRow key={index}>
+                                  <TableCell>
+                                      <Skeleton className="h-[20px] w-[150px]" />
+                                  </TableCell>
+                              </TableRow>
+                          ))}
+                </TableBody>
+            </Table>
+
+            {pagination ? (
+                <Pagination>
+                    <PaginationContent>
+                        <PaginationItem>
+                            {/* TODO: look into moving disabled code into a variant of the PaginationLink */}
+                            <PaginationPrevious
+                                className={
+                                    !pagination.prev
+                                        ? 'opacity-50 cursor-not-allowed'
+                                        : ''
+                                }
+                                aria-disabled={!pagination.prev}
+                                to={
+                                    pagination.prev
+                                        ? getPageLink(pagination.prev)
+                                        : '#'
+                                }
+                            />
+                        </PaginationItem>
+
+                        {/* TODO: add page numbers */}
+                        {/* <PaginationItem>
+                            <PaginationLink>1</PaginationLink>
+                        </PaginationItem>
+
+                        <PaginationItem>
+                            <PaginationLink isActive>2</PaginationLink>
+                        </PaginationItem>
+
+                        <PaginationItem>
+                            <PaginationEllipsis />
+                        </PaginationItem> */}
+
+                        <PaginationItem>
+                            <PaginationNext
+                                className={
+                                    !pagination.next
+                                        ? 'opacity-50 cursor-not-allowed'
+                                        : ''
+                                }
+                                aria-disabled={!pagination.next}
+                                to={
+                                    pagination.next
+                                        ? getPageLink(pagination.next)
+                                        : '#'
+                                }
+                            />
+                        </PaginationItem>
+                    </PaginationContent>
+                </Pagination>
+            ) : (
+                <div className="mx-auto flex w-full justify-center">
+                    <div className="flex flex-row items-center gap-1">
+                        <Skeleton className="h-10 w-20 rounded-md" />
+                        <Skeleton className="h-10 w-20 rounded-md" />
+                    </div>
+                </div>
+            )}
+        </section>
     );
 }
