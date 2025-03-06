71f8f16a06c200c89f150ce840733d186ffc09c4
nicholascannon <nicholascannon1@gmail.com>
2025-03-06 21:46:05 +1100

    Bit of menu bar work

--- src/components/menu-bar.tsx
+++ src/components/menu-bar.tsx
@@ -6,8 +6,18 @@ export function MenuBar() {
     return (
         <>
             <nav className="py-6 mb-6">
-                <ul className="mb-4">
-                    <Link to="/">Home</Link>
+                <ul className="flex gap-4 mb-4">
+                    <li>
+                        <h1 className="font-bold">Rick and Morty Characters</h1>
+                    </li>
+                    <li>
+                        <Link
+                            to="/"
+                            className="text-muted-foreground hover:text-primary"
+                        >
+                            Home
+                        </Link>
+                    </li>
                 </ul>
                 <Separator />
             </nav>
